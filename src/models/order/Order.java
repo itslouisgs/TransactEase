b8cc27003ab3b25169d4cbb8634203a970fc154f
Criez's <68372390+Criezc@users.noreply.github.com>
2023-01-16 18:01:36 +0700

    feat: create query on order & order detail

--- src/models/order/Order.java
+++ src/models/order/Order.java
@@ -1,18 +1,24 @@
 package models.order;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Vector;
 
+import database.DatabaseConnection;
 import template.payments.Payments;
 
 public class Order {
 	private Vector<OrderDetail> orders = new Vector<>();
 	private Payments payments;
+	private String id;
+	private DatabaseConnection con = DatabaseConnection.getInstance();
 	
 	public Order() {}
 	
-	public Order(Vector<OrderDetail> orders, Payments payments) {
+	public Order(Vector<OrderDetail> orders, Payments payments, String id) {
 		this.orders = orders;
 		this.payments = payments;
+		this.id = id;
 	}
 	
 	public Double calculateSubotal() {
@@ -58,4 +64,42 @@ public class Order {
 	public void setPayments(Payments payments) {
 		this.payments = payments;
 	}
+		
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public boolean insertOrder() {
+		String query = String.format("INSERT INTO Orders (id,payment_id, payment_type) VALUES (?, ?, ?)");
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		try {
+			ps.setString(1, id);
+			ps.setString(2, payments.getPaymentId());
+			ps.setString(3, payments.getPaymentName());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean delete() {
+		String query = String.format("DELETE FROM Orders WHERE id=?");
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		try {
+			ps.setString(1, id);
+			return ps.executeUpdate() == 1;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
 }
--- src/models/order/OrderDetail.java
+++ src/models/order/OrderDetail.java
@@ -1,10 +1,15 @@
 package models.order;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import database.DatabaseConnection;
 import models.products.Product;
 
 public class OrderDetail {
 	private Product product;
 	private int quantity;
+	private DatabaseConnection con = DatabaseConnection.getInstance();
+	private String id;
 
 	public OrderDetail(Product product) {
 		super();
@@ -34,4 +39,44 @@ public class OrderDetail {
 		subTotal = product.getPrice() * quantity;
 		return subTotal;
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public boolean insertOrder() {
+		String query = String.format("INSERT INTO OrderDetails (id,name, price,quantity) VALUES (?, ?, ?,?)");
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		try {
+			ps.setString(1, id);
+			ps.setString(2, product.getName());
+			ps.setInt(3, product.getPrice());
+			ps.setInt(4, quantity);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean delete() {
+		String query = String.format("DELETE FROM OrderDetails WHERE id=?");
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		try {
+			ps.setString(1, id);
+			return ps.executeUpdate() == 1;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+
 }
